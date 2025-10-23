b9196834dc49b2a2b62a71e867225c5e16be9a21
Amanrathi07 <amanrathi4321@gmail.com>
2025-10-23 13:52:14 +0530

    add example.git

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -3,9 +3,10 @@ import { PrismaClient } from "../generated/prisma/client";
 function prismaClientSingleton (){
     return new PrismaClient
 }
-
+      //@ts-ignore
 const prisma = globalThis.prisma ?? prismaClientSingleton()
 
+      //@ts-ignore
 if(process.env.NODE_ENV !== "production" ) globalThis.prisma = prisma
 
 export default prisma ;
